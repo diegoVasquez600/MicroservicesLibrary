c6f2772ab72dfff79782b4829f2c10582fb7a76c
Diego Rios Vasquez <diegovasquezcolombia@gmail.com>
2021-08-30 14:30:54 -0500

    Add project files.

---
+++ Librery.API/Core/Entities/Author.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Core.Entities
+{
+    public class Author
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string _Id { get; set; }
+        [BsonElement("Nombre")]
+        public string Nombre { get; set; }
+        [BsonElement("Apellido")]
+        public string Apellido { get; set; }
+        [BsonElement("gradoAcademico")]
+        public string GradoAcademico { get; set; }
+    }
+}
